7117a752b84394cb7bf3f27a0a076893b937a5e0
moumitabanik <banikmoumita99@gmail.com>
2023-12-27 23:19:47 +0530

    created static files

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -126,17 +126,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIR = {
-    os.path.join(BASE_DIR , "public/static")
-}
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 
