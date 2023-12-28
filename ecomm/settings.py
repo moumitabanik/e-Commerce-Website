6c123f8a94156e00c8310a3f87ca9ddb042444e3
moumitabanik <banikmoumita99@gmail.com>
2023-12-28 22:05:29 +0530

    added test smtp for email validation

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'moumeowbanik@gmail.com'
-EMAIL_HOST_PASSWORD = 'nwnnetstenqadili'
+EMAIL_HOST_USER = 'testmoumita4@gmail.com'
+EMAIL_HOST_PASSWORD = 'salkewkncxmbpdhb'
 
 
 
